465d8cb6a6500d947a7b51a6f00df8d5e2d953f9
Pecherkin Eugene <e.pecherkin@gmail.com>
2015-01-25 14:52:29 +0300

    use timer

--- chronograph.ino
+++ chronograph.ino
@@ -1,49 +1,84 @@
-const int segmentA = 12;
-const int segmentB = 10;
-const int segmentC = 13;
-const int segmentD = 16; // a2
-const int segmentE = 17; // a3
-const int segmentF = 11;
-const int segmentG = 14; // a0
-const int segmentH = 15; // a1
+#define uchar unsigned char
+#define uint  unsigned int
+
+const uchar segmentA = 12;
+const uchar segmentB = 10;
+const uchar segmentC = 13;
+const uchar segmentD = 16; // a2
+const uchar segmentE = 17; // a3
+const uchar segmentF = 11;
+const uchar segmentG = 14; // a0
+const uchar segmentH = 15; // a1
 
 // not a rank (не разряд)
-const int digit1   = 7;
-const int digit2   = 8;
-const int digit3   = 9;
-const int digit4   = 6;
+const uchar digit1   = 7;
+const uchar digit2   = 8;
+const uchar digit3   = 9;
+const uchar digit4   = 6;
 
-const int segmentsCount = 8;
-const int digitsCount = 4;
-const int segments[] = { segmentA, segmentB, segmentC, segmentD, segmentE, segmentF, segmentG, segmentH };
-const int digits[]   = { digit1, digit2, digit3, digit4 };
+const uchar segmentsCount = 8;
+const uchar digitsCount = 4;
+const uchar segments[] = { segmentA, segmentB, segmentC, segmentD, segmentE, segmentF, segmentG, segmentH };
+const uchar digits[]   = { digit1, digit2, digit3, digit4 };
 
-volatile unsigned long int time1 = 0;
-volatile unsigned long int time2 = 0;
+const uchar button = 4;
 
+volatile uchar s1 = 0;
+volatile uchar s2 = 0;
+
+volatile double length = 0.151;
 volatile double averageData = 0;
 volatile double lastData = 0;
 
-const int button = 4;
+void Timer1Init() {
+  TCNT1 = 0;
+
+  // Bits: COM1A1 COM1A0 COM1B1 COM1B0 - - WGM11 WGM10
+  TCCR1A = 0;
+
+  // Bits: ICNC1 ICES1 - WGM13 WGM12 CS12 CS11 CS10
+  TCCR1B = 0;
+
+  // Bits: FOC1A FOC1B - - - - - -
+  TCCR1C = 0;
+
+  // Bits: - - ICIE1 - - OCIE1B OCIE1A TOIE1
+  TIMSK1 = 0;
+
+  // Bits: – – ICF1 – - OCF1B OCF1A TOV1
+  TIFR1 = 0;
+}
+
 
 void sensor1() {
-  if(time1 == 0) {
-    time1 = micros();
+  if(s1 == 0) {
+    TCCR1B = (1<<CS10); // Timer/Counter 1 running (no prescaling)
+    s1 = 1;
   }
 }
 
 void sensor2() {
-  if(time2 == 0) {
-    time2 = micros();
+  if(s2 == 0) {
+    TCCR1B = 0; // Timer/Counter 1 stopped (no clock source)
+    s2 = 1;
   }
 }
 
 void setup() {
-  for(int i = 0; i < segmentsCount; ++i) {
+  // Global disable interrupts
+  cli();
+
+  // Timer/Counter 1 initialization
+  Timer1Init();
+
+  // Global enable interrupts
+  sei();
+
+  for(uchar i = 0; i < segmentsCount; ++i) {
     pinMode(segments[i], OUTPUT);
   }
 
-  for(int i = 0; i < digitsCount; ++i) {
+  for(uchar i = 0; i < digitsCount; ++i) {
     pinMode(digits[i], OUTPUT);
   }
 
@@ -65,19 +100,33 @@ void loop() {
 
 void realLoop() {
   outreset();
+
+  if(s1 && s2) {
+    double time = TCNT1 * (1.0 / 16000000.0);
+    averageData = ((averageData * 2) + lastData) / 3;
+    lastData = length / time;
+    s1 = s2 = 0;
+  }
+
   lastData = 567.3;
-  outvalue(lastData);
+  averageData = 213.4;
+
+  if(digitalRead(button)) {
+    outvalue(averageData);
+  } else {
+    outvalue(lastData);
+  }
 
   delay(3);
 }
 
 void sensorLoop() {
   outreset();
-  if(time1 > 0) {
+  if(s1) {
     outdigit(1, 0, 1);
   }
 
-  if(time2 > 0) {
+  if(s2) {
     outdigit(1, 0, 2);
   }
 
@@ -91,11 +140,11 @@ void sensorLoop() {
 void testLoop() {
   outreset();
 
-  /*for(int i = 0; i < segmentsCount; ++i) {*/
+  /*for(uchar i = 0; i < segmentsCount; ++i) {*/
     /*digitalWrite(segments[i], LOW);*/
   /*}*/
 
-  for(int i = 0; i < digitsCount; ++i) {
+  for(uchar i = 0; i < digitsCount; ++i) {
     digitalWrite(digits[i], HIGH);
   }
 
@@ -103,7 +152,7 @@ void testLoop() {
 
   /*delay(1000);*/
 
-  for(int i = 0; i < segmentsCount; ++i) {
+  for(uchar i = 0; i < segmentsCount; ++i) {
     digitalWrite(segments[i], LOW);
     delay(500);
     digitalWrite(segments[i], HIGH);
@@ -111,16 +160,16 @@ void testLoop() {
 }
 
 void outreset() {
-  for(int i = 0; i < segmentsCount; ++i) {
+  for(uchar i = 0; i < segmentsCount; ++i) {
     digitalWrite(segments[i], HIGH);
   }
 
-  for(int i = 0; i < digitsCount; ++i) {
+  for(uchar i = 0; i < digitsCount; ++i) {
     digitalWrite(digits[i], LOW);
   }
 }
 
-void outdigit(int value, int point, int digit) {
+void outdigit(uchar value, uchar point, uchar digit) {
   digitalWrite(digits[digit-1], HIGH);
 
   if(point)
@@ -199,15 +248,13 @@ void outdigit(int value, int point, int digit) {
   }
 }
 
-int counter = 1;
+uchar counter = 1;
 void outvalue(double value) {
-  int point = 0;
-  if(counter == 3)
-    point = 1;
+  uchar point = (counter == 3) ? 1 : 0;
 
-  int val = (int)(value*10);
-  int figure = 0;
-  for(int i = 1; i <= (5 - counter); ++i) {
+  uint val = (uint)(value*10);
+  uchar figure = 0;
+  for(uchar i = 1; i <= (5 - counter); ++i) {
     figure = val % 10;
     val = val / 10;
   }
